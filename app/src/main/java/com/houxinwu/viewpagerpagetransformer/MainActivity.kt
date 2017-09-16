23cfcf5211cf02948a5d3d5d6d88c0a0628afb5d
houxinwu <houxinwu@gmail.com>
2017-09-16 15:57:16 +0800

    去掉Fragment，添加indicator

--- app/src/main/java/com/houxinwu/viewpagerpagetransformer/MainActivity.kt
+++ app/src/main/java/com/houxinwu/viewpagerpagetransformer/MainActivity.kt
@@ -1,33 +1,58 @@
 package com.houxinwu.viewpagerpagetransformer
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        val ITEM_COUNT = 5
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val list: MutableList<Fragment> = mutableListOf()
-        for(it in 0..5) {
-            list.add(ViewPagerItemFragment.newInstance())
-        }
+        indicator.itemCount = ITEM_COUNT
 
-        viewPager.adapter = object : FragmentPagerAdapter(supportFragmentManager) {
-            override fun getItem(position: Int): Fragment {
-                return list[position]
+        viewPager.adapter = object : PagerAdapter() {
+            override fun instantiateItem(container: ViewGroup?, position: Int): Any {
+                val parent = FrameLayout(this@MainActivity)
+                val layoutParams = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT)
+                layoutParams.gravity = Gravity.CENTER
+                val image = ImageView(this@MainActivity)
+                image.setImageResource(R.drawable.bg_default_course)
+                image.layoutParams = layoutParams
+                parent.addView(image)
+                container?.addView(parent)
+                return parent
+            }
+
+            override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
+                return view == `object`
+            }
+
+            override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
+                if (`object` is View) {
+                    container?.removeView(`object`)
+                }
             }
 
             override fun getCount(): Int {
-                return list.size
+                return ITEM_COUNT
             }
+
         }
 
-        viewPager.offscreenPageLimit = 2
+        viewPager.offscreenPageLimit = 3
         viewPager.setPageTransformer(true) {
             page, position ->
             val MIN_SCALE = 0.9f
@@ -40,5 +65,19 @@ class MainActivity : AppCompatActivity() {
             page.scaleX = scale
             page.scaleY = scale
         }
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                indicator.setCurrent(position, positionOffset)
+            }
+
+            override fun onPageSelected(position: Int) {
+
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/houxinwu/viewpagerpagetransformer/ViewPagerIndicator.kt
@@ -0,0 +1,77 @@
+package com.houxinwu.viewpagerpagetransformer
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by houxinwu on 2017/9/16.
+ */
+class ViewPagerIndicator : View {
+    private lateinit var paint: Paint
+    private lateinit var center: PointF
+
+    private var currentPosition = 0
+    private var offset = 0f
+
+    private val itemHeight = 10f
+    private val itemWidth = 50f
+
+    var itemCount = 0
+
+    constructor(context: Context?) : super(context) {
+        init()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    fun init() {
+        paint = Paint()
+        paint.style = Paint.Style.STROKE
+        paint.strokeCap = Paint.Cap.ROUND
+
+        center = PointF()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        center.x = w.toFloat() / 2
+        center.y = h.toFloat() / 2
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), itemHeight.toInt())
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        val width = getBackgroundWidth()
+        paint.color = Color.parseColor("#d8d8d8")
+        paint.strokeWidth = itemHeight - 2f
+        canvas?.drawLine(center.x - width / 2, center.y, center.x + width/ 2, center.y, paint)
+
+        paint.color = Color.parseColor("#f32020")
+        paint.strokeWidth = itemHeight
+        val startX = center.x - width / 2 + itemWidth * (currentPosition % itemCount + offset)
+        canvas?.drawLine(startX, center.y, startX + itemWidth, center.y, paint)
+
+    }
+
+    private fun getBackgroundWidth(): Float {
+        return itemWidth * itemCount
+    }
+
+    public fun setCurrent(position: Int, offset: Float) {
+        this.currentPosition = position
+        this.offset = offset
+        invalidate()
+    }
+}
